2fed098799f5272830ec1db889b82decdaa60673
mgongar2703 <92602289+mgongar2703@users.noreply.github.com>
2022-10-14 17:54:27 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Miguel González García</title>
+</head>
+<body>
+<div>
+<h1>Welcome to Rock Paper Scissors</h1>
+</p>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+</div>
+</body>
---
+++ login.php
@@ -0,0 +1,61 @@
+<?php 
+
+if ( isset($_POST['cancel'] ) ) {
+
+    header("Location: index.php");
+    return;
+}
+
+$salt = 'XyZzy12*_';
+$stored_hash = '1a52e17fa899cf40fb04cfc42e6352f1';  
+
+$failure = false; 
+
+if ( isset($_POST['who']) && isset($_POST['pass']) ) {
+    if ( strlen($_POST['who']) < 1 || strlen($_POST['pass']) < 1 ) {
+        $failure = "User name and password are required";
+    } else {
+        $check = hash('md5', $salt.$_POST['pass']);
+        if ( $check == $stored_hash ) {
+           
+            header("Location: game.php?name=".urlencode($_POST['who']));
+            return;
+        } else {
+            $failure = "Incorrect password";
+        }
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Miguel González García</title>
+</head>
+<body>
+<div>
+<h1>Please Log In</h1>
+<?php
+
+if ( $failure !== false ) {
+   
+    echo('<p style="color: red;">'.htmlentities($failure)."</p>\n");
+}
+?>
+<form method="POST">
+<label for="nam">User Name</label>
+<input type="text" name="who" id="nam"><br/>
+<label for="id_1723">Password</label>
+<input type="text" name="pass" id="id_1723"><br/>
+<input type="submit" value="Log In">
+<input type="submit" name="cancel" value="Cancel">
+</form>
+<p>
+For a password hint, view source and find a password hint
+in the HTML comments.
+<!-- Hint: The password is the 3  character language we are learning (all lower case) followed by 123. -->
+</p>
+</div>
+</body>
